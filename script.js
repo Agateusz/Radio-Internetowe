bfca8a598c1f6d2e033b6da6b47e4cd67b8db258
Agateusz <71345438+Agateusz@users.noreply.github.com>
2026-03-29 20:04:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const statsUrl = "https://somafm.com/songs/groovesalad.json";
+
+async function updateMetadata() {
+    try {
+        const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(statsUrl)}`);
+        const json = await response.json();
+        const data = JSON.parse(json.contents);
+        
+        // Wyciągamy ostatnią piosenkę z listy
+        const current = data.songs[0];
+        
+        document.getElementById('artist-name').innerText = current.artist;
+        document.getElementById('song-title').innerText = current.title;
+        
+        console.log("Gra: " + current.artist + " - " + current.title);
+    } catch (error) {
+        console.error("Błąd:", error);
+        document.getElementById('song-title').innerText = "Radio Alfa Live";
+    }
+}
+
+setInterval(updateMetadata, 30000); // Odświeżaj co 30 sekund
+updateMetadata();
